58dd6ade556daf48ae33fbe3d90dc43d7abb904d
cgallanty <31599446+cgallanty@users.noreply.github.com>
2017-09-17 21:06:34 -0400

    Lab3-SQL Queries
    Christopher Gallanty
    Lab3-SQL Queries
    09/17/17

---
+++ SQL Queries.sql
@@ -0,0 +1,43 @@
+﻿-- Query 1 --
+select ordNo, totalUSD
+from Orders;
+
+-- Query 2 --
+select name, city
+from agents
+where aid in ('a01');
+
+-- Query 3 --
+select pid, name, priceUSD
+from products
+where qty>200010;
+
+-- Query 4 --
+select name, city
+from customers
+where city in ('Duluth');
+
+-- Query 5--
+select name
+from agents
+where city not in ('New York','Duluth');
+
+-- Query 6 --
+select pid, name, city, quantity, priceUSD
+from products
+where city not in ('Duluth','Dallas') and priceUSD >= 1;
+
+-- Query 7 --
+select ordNumber, month, cid, aid, pid, qty, totalUSD
+from orders
+where month in ('March','May');
+
+-- Query 8 --
+select ordNumber, month, cid, aid, pid, qty, totalUSD
+from orders
+where month in ('Feb') and totalUSD >= 500;
+
+-- Query 9 --
+select ordNumber
+from orders
+where cid in ('C005');
